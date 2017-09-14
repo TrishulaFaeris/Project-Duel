f68a5867c1cc022e7b997adc5774eb377df75743
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-09-14 11:44:39 -0400

    something with CharA

--- Project Duel KevinAndrew/src/main/CharacterA.java
+++ Project Duel KevinAndrew/src/main/CharacterA.java
@@ -1,5 +1,27 @@
 package main;
 
 public class CharacterA implements Dueler{
-
+	private int hp;
+	public CharacterA() {
+	}
+	public void taunt() {
+		
+		System.out.println("Pew pew pew");
+	}
+	public String getName() {
+		
+		return "Trishula";
+	}
+	public void setStartingHP(int hp) {
+		
+		this.hp = hp;
+	}
+	public int getHP() {
+		
+		return hp;
+	}
+	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
+		
+		return false;
+	};
 }
