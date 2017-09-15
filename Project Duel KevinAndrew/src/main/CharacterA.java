053fc39343c7acf5420b8765f5caf347d33c1c11
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-09-15 11:48:13 -0400

    push

--- Project Duel KevinAndrew/src/main/CharacterA.java
+++ Project Duel KevinAndrew/src/main/CharacterA.java
@@ -5,24 +5,31 @@ public class CharacterA implements Dueler{
 	public CharacterA() {
 	}
 	public void taunt() {
-		
 		System.out.println("Pew pew pew");
 	}
 	public String getName() {
-		
 		return "Trishula";
 	}
 	public void setStartingHP(int hp) {
-		
 		this.hp = hp;
 	}
 	public int getHP() {
-		
 		return hp;
 	}
 	public boolean determineIfOpponentIsFair(Dueler d, int hp) {
 		if(d.getHP() == hp)
 			return true;
 		return false;
+	}
+	public int getAction(Object caller) {
+		if(caller instanceof Dueler)
+			return 3;
+		else
+		{
+			return 0;
+		}
+	}
+	public void hit(Object caller) {
+		
 	};
 }
